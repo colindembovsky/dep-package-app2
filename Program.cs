7bc150e0afae7ccdeac0ef307c16757f3b421de3
Colin Dembovsky <colindembovsky@gmail.com>
2021-11-10 15:57:40 +0000

    Creating app that consumes package

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using superlib1;
+
+namespace dep_package_app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var calc = new Class1();
+            var res = calc.Add(1, 2);
+            Console.WriteLine($"Result of 1 + 2 is {res}");
+        }
+    }
+}
